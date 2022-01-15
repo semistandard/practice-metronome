e48ab3ae97ee352c035ab53c52441028de6f21d9
semistandard <71646868+semistandard@users.noreply.github.com>
2022-01-15 19:04:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,6 @@ import streamlit as st
 import sys
 
 st.title('指板の音階を覚えるためのメトロノーム')
-import pyaudio 
-import numpy as np
- # 音声を出力するためのストリームを開く --- (*1) 
-p = pyaudio.PyAudio() 
-stream = p.open(format=pyaudio.paFloat32, channels=1, rate=44100, frames_per_buffer=1024, output=True) 
-# 適当なサイン波を生成する --- (*2) 
-samples = np.sin(np.arange(50000) / 20) 
-# サイン波を再生する --- (*3) 
-print("play") 
-stream.write(samples.astype(np.float32).tostring()) 
-stream.close()
 
 #設定
 beat = st.slider('拍子', 1, 10, 4)
